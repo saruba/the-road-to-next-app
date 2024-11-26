e55582f9d5589b30c1d7d2f33995e83e45f8c5d4
Salva <saruba@gmail.com>
2024-11-26 16:19:19 +0100

    update features

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,14 +1,15 @@
 import { Heading } from "@/components/heading";
-import { initialTickets } from "@/data";
-import { TicketItem } from "@/features/components/ticket-item";
+import { TicketItem } from "@/features/tickets/components/ticket-item";
+import { getTickets } from "@/features/tickets/queries/get-tickets";
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
+  const tickets = await getTickets();
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -20,7 +20,7 @@ const ThemeSwitcher = () => {
       />
       <MoonIcon
         className="
-          absolute h-4 w-4 rotate-0 scale-100 transition-transform
+          absolute h-4 w-4 rotate-90 scale-0 transition-transform
           dark:rotate-0 dark:scale-100
         "
       />
