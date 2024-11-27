8ff859a8db0531acc20bcf889525038e8b6df4ba
Salva <saruba@gmail.com>
2024-11-27 18:55:33 +0100

    actions

--- src/features/tickets/components/ticket-item.tsx
+++ src/features/tickets/components/ticket-item.tsx
@@ -1,10 +1,11 @@
 import { Ticket } from "@prisma/client";
 import clsx from "clsx";
-import { SquareArrowOutUpRightIcon } from "lucide-react";
+import { SquareArrowOutUpRightIcon, TrashIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
 
 type TicketProps = {
@@ -19,6 +20,14 @@ const TicketItem = ({ ticket, isDetail }: TicketProps) => {
         <SquareArrowOutUpRightIcon className="h-4 w-4" />
       </Link>
     </Button>
+  );
+
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon">
+        <TrashIcon className="h-4 w-4" />
+      </Button>
+    </form>
   );
   return (
     <div
@@ -44,7 +53,9 @@ const TicketItem = ({ ticket, isDetail }: TicketProps) => {
           </span>
         </CardContent>
       </Card>
-      {!isDetail && <div className="flex flex-col gap-y-1">{detailButton}</div>}
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? deleteButton : detailButton}
+      </div>
     </div>
   );
 };
